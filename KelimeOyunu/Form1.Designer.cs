b9d483cf4f27813080544ca235b019de27f7abe3
harundroid <ozanmoral@gmail.com>
2022-05-24 06:14:13 +0300

    Proje dosyası ekle.

---
+++ KelimeOyunu/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace KelimeOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.question = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.answerA = new System.Windows.Forms.Button();
+            this.answerB = new System.Windows.Forms.Button();
+            this.answerC = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.question, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // question
+            // 
+            this.question.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.question.Font = new System.Drawing.Font("Microsoft YaHei UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.question.Location = new System.Drawing.Point(23, 3);
+            this.question.Name = "question";
+            this.question.Size = new System.Drawing.Size(754, 89);
+            this.question.TabIndex = 0;
+            this.question.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.answerA, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.answerB, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.answerC, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(23, 115);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(754, 332);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // answerA
+            // 
+            this.answerA.BackColor = System.Drawing.Color.White;
+            this.answerA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.answerA.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.answerA.FlatAppearance.BorderSize = 3;
+            this.answerA.FlatAppearance.CheckedBackColor = System.Drawing.Color.Aqua;
+            this.answerA.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DeepSkyBlue;
+            this.answerA.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
+            this.answerA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.answerA.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.answerA.Location = new System.Drawing.Point(3, 3);
+            this.answerA.Name = "answerA";
+            this.answerA.Size = new System.Drawing.Size(748, 104);
+            this.answerA.TabIndex = 0;
+            this.answerA.Text = "button1";
+            this.answerA.UseVisualStyleBackColor = false;
+            this.answerA.Click += new System.EventHandler(this.answerA_Click);
+            // 
+            // answerB
+            // 
+            this.answerB.BackColor = System.Drawing.Color.White;
+            this.answerB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.answerB.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.answerB.FlatAppearance.BorderSize = 3;
+            this.answerB.FlatAppearance.CheckedBackColor = System.Drawing.Color.Aqua;
+            this.answerB.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DeepSkyBlue;
+            this.answerB.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
+            this.answerB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.answerB.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.answerB.Location = new System.Drawing.Point(3, 113);
+            this.answerB.Name = "answerB";
+            this.answerB.Size = new System.Drawing.Size(748, 104);
+            this.answerB.TabIndex = 1;
+            this.answerB.Text = "button1";
+            this.answerB.UseVisualStyleBackColor = false;
+            this.answerB.Click += new System.EventHandler(this.answerB_Click);
+            // 
+            // answerC
+            // 
+            this.answerC.BackColor = System.Drawing.Color.White;
+            this.answerC.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.answerC.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.answerC.FlatAppearance.BorderSize = 3;
+            this.answerC.FlatAppearance.CheckedBackColor = System.Drawing.Color.Aqua;
+            this.answerC.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DeepSkyBlue;
+            this.answerC.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
+            this.answerC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.answerC.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.answerC.Location = new System.Drawing.Point(3, 223);
+            this.answerC.Name = "answerC";
+            this.answerC.Size = new System.Drawing.Size(748, 106);
+            this.answerC.TabIndex = 2;
+            this.answerC.Text = "button1";
+            this.answerC.UseVisualStyleBackColor = false;
+            this.answerC.Click += new System.EventHandler(this.answerC_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Kelime Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox question;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button answerA;
+        private System.Windows.Forms.Button answerB;
+        private System.Windows.Forms.Button answerC;
+    }
+}
+
---
+++ KelimeOyunu/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KelimeOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private DataTable GetWords()
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(this.GetType().Assembly.GetManifestResourceStream("KelimeOyunu.Kelimeler.xml"));
+            DataTable table = new DataTable();
+            table.Columns.Add("ID");
+            table.Columns.Add("Ing");
+            table.Columns.Add("TR");
+            DataRow newRow = table.NewRow();
+
+            int column = 0;
+            for (int i = 0; i < ds.Tables[1].Rows.Count; i++)
+            {
+                DataRow currentRow = ds.Tables[1].Rows[i];
+                //DataRow exRow = ds.Tables[1].Rows[i > 0 ? i - 1 : 0];
+                //if (exRow[1].ToString() == currentRow[1].ToString())
+                {
+                    switch (column)
+                    {
+                        case 0:
+                            newRow[0] = currentRow[0];
+                            column++;
+                            break;
+                        case 1:
+                            newRow[1] = currentRow[0];
+                            column++;
+                            break;
+                        case 2:
+                            newRow[2] = currentRow[0];
+                            table.Rows.Add(newRow);
+                            column = 0;
+                            newRow = table.NewRow();
+                            break;
+                    }
+                }
+            }
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                table.Rows[i][0] = i + 1;
+            }
+            return table;
+        }
+        void GetQuestion()
+        {
+            Random random = new Random();
+            questionRow = words.Rows[random.Next(words.Rows.Count)];//Soru Bulunuyor
+
+            this.question.Text = questionRow[1].ToString();//Soru Yazılıyor.
+            int ans = random.Next(2);//Cevap butonu bulunuyor.
+            buttons[ans].Text = questionRow[2].ToString();//Cevap butona yazılıyor
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                if (ans == i) continue;//Cevap butonu ayıklanıyor
+                else buttons[i].Text = words.Rows[random.Next(words.Rows.Count)][2].ToString();//Yanlış cevaplar yazılıyor.
+            }
+        }
+        DataTable words = new DataTable();
+        List<Button> buttons = new List<Button>();
+        DataRow questionRow { get; set; }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttons.Add(answerA);
+            buttons.Add(answerB);
+            buttons.Add(answerC);
+            words = GetWords();
+            GetQuestion();//Soru çağırılıyor.
+        }
+        void DogruCevap()
+        {
+            question.Text = "Doğru Cevap:)";
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                buttons[i].Text = "Devam...";
+                buttons[i].Enabled = true;
+                buttons[i].BackColor=Color.White;
+                buttons[i].ForeColor = Color.Black;
+            }
+        }
+        private void answerA_Click(object sender, EventArgs e)
+        {
+            if (answerA.Text.ToString() == "Devam...")
+            {
+                GetQuestion();
+            }
+            else
+            {
+                if (answerA.Text.ToString() == questionRow[2].ToString())
+                {
+                    DogruCevap();
+                }
+                else
+                {
+                    //SolidColorBrush b = new SolidColorBrush();
+                    //b.Color = Colors.Red;
+                    //answerA.Foreground = b;
+                    //answerA.IsEnabled = false;
+                    answerA.ForeColor = Color.WhiteSmoke;
+                    answerA.BackColor = Color.Red;
+                    answerA.Enabled = false;
+                }
+            }
+        }
+
+        private void answerB_Click(object sender, EventArgs e)
+        {
+            if (answerB.Text.ToString() == "Devam...")
+            {
+                GetQuestion();
+            }
+            else
+            {
+                if (answerB.Text.ToString() == questionRow[2].ToString())
+                {
+                    DogruCevap();
+                }
+                else
+                {
+                    //SolidColorBrush b = new SolidColorBrush();
+                    //b.Color = Colors.Red;
+                    //answerA.Foreground = b;
+                    //answerA.IsEnabled = false;
+                    answerB.ForeColor = Color.WhiteSmoke;
+                    answerB.BackColor = Color.Red;
+                    answerB.Enabled = false;
+                }
+            }
+        }
+
+        private void answerC_Click(object sender, EventArgs e)
+        {
+            if (answerC.Text.ToString() == "Devam...")
+            {
+                GetQuestion();
+            }
+            else
+            {
+                if (answerC.Text.ToString() == questionRow[2].ToString())
+                {
+                    DogruCevap();
+                }
+                else
+                {
+                    //SolidColorBrush b = new SolidColorBrush();
+                    //b.Color = Colors.Red;
+                    //answerA.Foreground = b;
+                    //answerA.IsEnabled = false;
+                    answerC.ForeColor = Color.WhiteSmoke;
+                    answerC.BackColor = Color.Red;
+                    answerC.Enabled = false;
+                }
+            }
+        }
+    }
+}
